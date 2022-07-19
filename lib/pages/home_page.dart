79022c3fdd6ae84f7af0c8ef15f292638a960a5c
cqiang102 <lacia.cq@qq.com>
2022-07-19 18:45:57 +0800

    init test page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(color: Colors.blue.shade100,child: Center(child: TextButton(onPressed: () {  },
+    child: Text('登陆'),),),);
   }
 }
